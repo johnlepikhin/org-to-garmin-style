3305b50c91a22351f6095d65e77a20be8fac7709
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-11-29 22:58:26 +0300

    Improvements
     1. Generate ready-to-use style.zip
     2. Fix generator of mkgmap-args.txt
     3. Output brief HOW-TO when done.

--- org-to-style.pl
+++ org-to-style.pl
@@ -20,6 +20,7 @@ if (!defined $org_file || !defined $output_directory) {
 }
 
 my $codepage = 1251;
+my $style_name = 'generated';
 
 binmode STDOUT, ':utf8';
 
@@ -28,8 +29,10 @@ my $orgp = Org::Parser->new();
 # parse a file
 my $doc = $orgp->parse_file( $org_file );
 
-
-mkdir $output_directory;
+my $style_directory = "$output_directory/styles/$style_name";
+foreach ($output_directory, "$output_directory/styles", $style_directory) {
+    mkdir $_;
+}
 
 my ($org_basedir) = $org_file =~ m{(.*)/};
 $org_basedir //= './';
@@ -54,7 +57,7 @@ sub getfh ($) {
     else {
         croak "Uknown type: $_[0]";
     }
-    open my $fh, '>:utf8', "$output_directory/$fname";
+    open my $fh, '>:utf8', "$style_directory/$fname";
     $out{ $_[0] } = $fh;
     return $fh;
 }
@@ -75,9 +78,9 @@ $doc->walk(
         elsif ( defined $heading ) {
             if ( $el->isa('Org::Element::Link') ) {
                 my $link = $el->link();
-                if ( $link =~ /\.xpm$/ ) {
-                    if ( $link =~ /^file:(.*)/ ) {
-                        $heading->{xpm} = "$org_basedir/$1";
+                if ( $link =~ m{\.xpm$} ) {
+                    if (my ($file) = $link =~ /^file:(.*)/ ) {
+                        $heading->{xpm} = "$org_basedir/$file";
                     }
                     else {
                         carp "Only file: links are supported for XPMs: $link";
@@ -131,7 +134,7 @@ my $fid = $props{FAMILY_ID} // croak "Cannot get FAMILY_ID from file header";
 
 # my $pid = $props{PRODUCT_ID} // 1;
 my $version = $props{VERSION} // 1;
-write_file( "$output_directory/version", "$version\n" );
+write_file( "$style_directory/version", "$version\n" );
 
 open $fh, ">:encoding(cp$codepage)", "$output_directory/style.txt";
 print $fh <<"END"
@@ -195,6 +198,11 @@ END
 
 close $fh;
 
+if (! chdir $output_directory) {
+    croak "Cannot chdir() to $output_directory";
+}
+system 'zip', '-qr9', "$output_directory/style.zip", 'styles';
+
 open $fh, ">", "$output_directory/mkgmap-args.txt";
 print $fh <<"END"
 generate-sea: land-tag=natural=background
@@ -215,15 +223,30 @@ route
 index
 nsis
 gmapsupp
-style-file=$output_directory
 unicode
-family-id=$fid
-product-id=1
-code-page=$codepage
+family-id: $fid
+product-id: 1
+code-page: $codepage
+style-file: $output_directory/style.zip
+style: $style_name
+input-file: style.txt
 END
   ;
 
 close $fh;
+
+print <<"END"
+DONE. Typical next steps:
+
+ 1. mkdir /tmp/temp-dir && cd /tmp/temp-dir
+ 2. splitter.jar --num-tiles=4 --mapid=12345678 --keep-complete=false /path/to/map.osm
+ 3. mkgmap -c $output_directory/mkgmap-args.txt --description='my map' *.pbf
+
+Or without splitting:
+
+ 1. mkdir /tmp/temp-dir && cd /tmp/temp-dir
+ 2. mkgmap -c $output_directory/mkgmap-args.txt -n 12345678 --description='my map' /path/to/map.osm
+END
 
 __END__
 
@@ -242,6 +265,6 @@ This is an attempt to simplify developement of mkgmap style files. All
 manual workflow is done using Emacs org-mode and any graphical editor
 which support XPM (I use Gimp).
 
-For more info see:
+For details see:
 
 https://johnlepikhin.github.io/blog/2017/11/04/создание-своих-карт-для-gps
